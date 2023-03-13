f09d840126be4a800d998423bf1755d741f28dc0
David Espino <davidespinog@gmail.com>
2023-03-13 17:47:20 -0600

    Update createMovieHtml.js

--- src/modules/createMovieHtml.js
+++ src/modules/createMovieHtml.js
@@ -1,11 +1,16 @@
 const createMovieHtml = (obj) => {
   const container = document.createElement('div');
   const img = document.createElement('img');
-  const titleDiv = document.createElement('div');
   const title = document.createElement('p');
   const likeButton = document.createElement('i');
   const commentButton = document.createElement('button');
   const reserveButton = document.createElement('button');
   title.innerText = obj.title;
+  commentButton.innerText = 'Comment';
+  reserveButton.innerText = 'Reserve';
+  likeButton.classList.add('lar');
+  likeButton.classList.add('la-heart');
+
+  container.append(img, title, likeButton, commentButton, reserveButton);
   return container;
 };
