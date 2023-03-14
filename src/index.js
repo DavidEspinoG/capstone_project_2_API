317c528134ff3405a53b84efb66886a05952c4cb
Matt <mwbertolucci@gmail.com>
2023-03-14 12:03:54 -0300

    Add getMovie

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.scss';
+import getMovie from  './modules/getMovie.js';
+const movie = document.querySelector('.reservation-movie');
+
+movie.addEventListener('click', () => {
+    console.log('click')
+    getMovie(157336);
+});
