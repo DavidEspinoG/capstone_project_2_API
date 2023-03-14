08ff7661523f5da69d356cd6ec4df015d5498e2d
Matt <mwbertolucci@gmail.com>
2023-03-14 12:55:42 -0300

    Fix linters

--- src/modules/getMovie.js
+++ src/modules/getMovie.js
@@ -2,10 +2,10 @@ import mainUrl from './mainUrl.js';
 import apiKey from './apiKey.js';
 
 const getMovie = async (id) => {
-    const response = await fetch(`${mainUrl}movie/${id}?api_key=${apiKey}`);
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
+  const response = await fetch(`${mainUrl}movie/${id}?api_key=${apiKey}`);
+  const data = await response.json();
+  console.log(data)
+  return data;
+};
 
 export default getMovie;
