e6eea4d164a11ac7be2b93960a4856b9bfc66531
David Espino <davidespinog@gmail.com>
2023-03-14 14:01:33 -0600

    Add one more test for movieCounter

--- src/tests/movieCounter.test.js
+++ src/tests/movieCounter.test.js
@@ -8,7 +8,7 @@ test('movieCounter with 1 element', () => {
   const movies = document.querySelectorAll('.movie-card');
   expect(movieCounter(movies)).toBe(1);
 });
-test('movieCounter with 2 element', () => {
+test('movieCounter with 2 elements', () => {
   document.body.innerHTML = `
   <div class="movie-card"></div>
   <div class="movie-card"></div>
@@ -16,4 +16,14 @@ test('movieCounter with 2 element', () => {
   `;
   const movies = document.querySelectorAll('.movie-card');
   expect(movieCounter(movies)).toBe(2);
+});
+test('movieCounter with 3 element', () => {
+  document.body.innerHTML = `
+  <div class="movie-card"></div>
+  <div class="movie-card"></div>
+  <div class="movie-card"></div>
+  <span id="total-movies"></span>
+  `;
+  const movies = document.querySelectorAll('.movie-card');
+  expect(movieCounter(movies)).toBe(3);
 });
