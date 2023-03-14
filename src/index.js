a3a57c5e2a9995423181309b3ef6f3ac0877e1ab
David Espino <davidespinog@gmail.com>
2023-03-13 21:24:56 -0600

    Add styling to homepage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './style.scss';
-import getTrending from './modules/getTrending';
+import getTrending from './modules/getTrending.js';
+import createMovieHtml from './modules/createMovieHtml.js';
+
+const movieContainer = document.getElementById('movies-container');
 
 getTrending()
+  .then(data => {
+    data.forEach(element => {
+     movieContainer.append(createMovieHtml(element)) 
+    });
+  }).catch(error => console.log(error));
--- src/modules/createMovieHtml.js
+++ src/modules/createMovieHtml.js
@@ -3,14 +3,22 @@ const createMovieHtml = (obj) => {
   const img = document.createElement('img');
   const title = document.createElement('p');
   const likeButton = document.createElement('i');
+  const likes = document.createElement('span');
   const commentButton = document.createElement('button');
   const reserveButton = document.createElement('button');
+  container.classList.add('movie-card')
   title.innerText = obj.title;
+  likes.innerText = '1';
   commentButton.innerText = 'Comment';
+  commentButton.classList.add('button');
   reserveButton.innerText = 'Reserve';
+  reserveButton.classList.add('button');
   likeButton.classList.add('lar');
   likeButton.classList.add('la-heart');
-
+  likeButton.classList.add('like-button');
+  img.src = 'https://image.tmdb.org/t/p/w500/' + obj.poster_path;
   container.append(img, title, likeButton, commentButton, reserveButton);
   return container;
 };
+
+export default createMovieHtml;
