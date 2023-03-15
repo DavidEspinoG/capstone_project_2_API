008f294bbbfb02d156c663e45b90a04929e78f4b
getssh <gtamrat33@gmail.com>
2023-03-15 11:45:12 +0300

    Refactor commentPopUp function

--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -13,21 +13,50 @@ const movieData = async (movieId) => {
 
 const commentPopUp = (movieId) => {
   movieData(movieId).then(
-    (value) => { commentWrapper.innerHTML = `
-      <img src=${showImage}/${value.poster_path}>
-      <h2>${value.original_title}</h2>
-      <p>${value.overview}</p>
-      <h3>Add a comment</h3>
-      <input type="text" placeholder="Your name">
-      <textarea placeholder="Your insights"></textarea>
-      <button>Comment</button>
-      <button class="exit" onclick="closePopUp()">X</button>
-      `;
+    (value) => {
+      commentWrapper.innerHTML = '';
+      const popImg = document.createElement('img');
+      const popTitle = document.createElement('h2');
+      const popOverview = document.createElement('p');
+      const commnetListTitle = document.createElement('h3');
+      const commentLists = document.createElement('ul');
+      const commentList = document.createElement('li');
+      const commentTitle = document.createElement('h3');
+      const commnetForm = document.createElement('form');
+      const userName = document.createElement('input');
+      const commentText = document.createElement('textarea');
+      const submitBtn = document.createElement('button');
+      const exitBtn = document.createElement('button');
+
+      const iamgeUrl = `${showImage}/${value.backdrop_path}`;
+      popImg.setAttribute('src', iamgeUrl);
+      popTitle.innerText = value.original_title;
+      popOverview.innerText = value.overview;
+      commnetListTitle.innerText = 'Comments';
+      commentTitle.innerText = 'Add a comment';
+      commnetForm.className = 'comment-form';
+      userName.setAttribute('placeholder', 'Your Name');
+      userName.setAttribute('type', 'text');
+      commentText.setAttribute('placeholder', 'Comment');
+      submitBtn.className = 'submit-comment';
+      submitBtn.innerText = 'Comment';
+      exitBtn.className = 'exit-btn';
+      exitBtn.innerText = 'X';
+      // delete this place holder
+      const commentPlaceHolder = document.createElement('p');
+      commentPlaceHolder.innerText = '03/15/2023 Alex: I\'d love to watch this movie';
+
+      commnetForm.append(userName, commentText, submitBtn);
+      commentWrapper.append(popImg, popTitle, popOverview, commnetListTitle, commentPlaceHolder,
+        commentTitle, commnetForm, exitBtn);
+
+      exitBtn.addEventListener('click', () => {
+        commentWrapper.style.display = 'none';
+      });
     },
   );
 };
 
-function closePopUp() {
-  commentWrapper.style.display = 'none';
-}
+
+
 export default commentPopUp;
