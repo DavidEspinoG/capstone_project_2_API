c972a8cdc47f91c8bd7a50e32f05bd51afbe230d
getssh <gtamrat33@gmail.com>
2023-03-15 14:27:36 +0300

    Add GET and POST methods and fetch data from involvement api

--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -1,14 +1,46 @@
 import apiKey from '../apiKey.js';
-import '../comment.css';
+import '../comment.scss';
 
 const commentWrapper = document.querySelector('.comment-container');
 const endPoint = 'https://api.themoviedb.org/3/movie/';
 const showImage = 'http://image.tmdb.org/t/p/w500/';
+const appId = 'XXyTDvZv9uOyCyOJ39gw';
+const envolvAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
 
 const movieData = async (movieId) => {
   const response = await fetch(`${endPoint}/${movieId}?api_key=${apiKey}`);
   const data = await response.json();
   return data;
+};
+
+const newComment = async (itemId, username, comment) => {
+  let response;
+  try {
+    response = await fetch(`${envolvAPI}/${appId}/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: itemId,
+        username,
+        comment,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (response.status === 201) {
+    console.log('Post successful');
+  } else {
+    console.log('Post failed');
+  }
+};
+
+const getComment = async (movieId) => {
+  const response = await fetch(`${envolvAPI}/${appId}/comments?item_id=${movieId}`);
+  return response.json();
 };
 
 const commentPopUp = (movieId) => {
@@ -20,7 +52,7 @@ const commentPopUp = (movieId) => {
       const popOverview = document.createElement('p');
       const commnetListTitle = document.createElement('h3');
       const commentLists = document.createElement('ul');
-      const commentList = document.createElement('li');
+      commentLists.className = 'comment-list';
       const commentTitle = document.createElement('h3');
       const commnetForm = document.createElement('form');
       const userName = document.createElement('input');
@@ -32,7 +64,6 @@ const commentPopUp = (movieId) => {
       popImg.setAttribute('src', iamgeUrl);
       popTitle.innerText = value.original_title;
       popOverview.innerText = value.overview;
-      commnetListTitle.innerText = 'Comments';
       commentTitle.innerText = 'Add a comment';
       commnetForm.className = 'comment-form';
       userName.setAttribute('placeholder', 'Your Name');
@@ -43,12 +74,33 @@ const commentPopUp = (movieId) => {
       exitBtn.className = 'exit-btn';
       exitBtn.innerText = 'X';
       // delete this place holder
-      const commentPlaceHolder = document.createElement('p');
-      commentPlaceHolder.innerText = '03/15/2023 Alex: I\'d love to watch this movie';
+      // const commentPlaceHolder = document.createElement('p');
+      // commentPlaceHolder.innerText = '03/15/2023 Alex: I\'d love to watch this movie';
+
+      getComment(movieId).then(
+        (value) => {
+          const commentLength = value.length ? value.length : 0;
+          commnetListTitle.innerText = `Comments (${commentLength})`;
+          for (let i = 0; i < value.length; i += 1) {
+            const listItem = document.createElement('li');
+            listItem.innerText = `${value[i].creation_date} ${value[i].username}: ${value[i].comment}`;
+            commentLists.appendChild(listItem);
+          }
+        },
+      );
 
       commnetForm.append(userName, commentText, submitBtn);
-      commentWrapper.append(popImg, popTitle, popOverview, commnetListTitle, commentPlaceHolder,
+      commentWrapper.append(popImg, popTitle, popOverview, commnetListTitle, commentLists,
         commentTitle, commnetForm, exitBtn);
+
+      commentWrapper.style.display = 'flex';
+
+      commnetForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        newComment(movieId, userName.value, commentText.value);
+        userName.value = '';
+        commentText.value = '';
+      });
 
       exitBtn.addEventListener('click', () => {
         commentWrapper.style.display = 'none';
@@ -56,7 +108,5 @@ const commentPopUp = (movieId) => {
     },
   );
 };
-
-
 
 export default commentPopUp;
